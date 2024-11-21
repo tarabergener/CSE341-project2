0005ed5ee390d09cc54d15937b0c2d73691047a5
Tara Bergener <tara.kelly211@gmail.com>
2024-11-21 09:26:05 -0500

    gitignore

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+const port = 3030;
+
 app.get('/', (req, res) =>{
     res.send('Hello');
 });
 
-const port = 3030;
+app.use('/', require('./routes'));
 
 app.listen(process.env.port || port);
 console.log('Web Server is listening on port ' + (process.env.PORT || port))
