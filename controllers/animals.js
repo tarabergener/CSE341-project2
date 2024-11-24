3292f0533a5f91c97e1441535c7fde416ebbc60f
Tara Bergener <tara.kelly211@gmail.com>
2024-11-23 23:20:10 -0500

    animal not declared

--- controllers/animals.js
+++ controllers/animals.js
@@ -2,7 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    //#swagger.tags=['Users']
+    //#swagger.tags=['Animals']
     const result = await mongodb.getDatabase().db().collection('animals').find();
     result.toArray().then((animals) => {
         res.setHeader('Content-Type', 'application/json');
@@ -11,9 +11,9 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    //#swagger.tags=['Users']
+    //#swagger.tags=['Animals']
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('animals').find({ _id: userId }, animal);
+    const result = await mongodb.getDatabase().db().collection('animals').find({ _id: userId });
     result.toArray().then((animals) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(animals[0]);
