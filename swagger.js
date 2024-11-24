0142cb1293633ee7b1fd023453e7c7f457bf165c
Tara Bergener <tara.kelly211@gmail.com>
2024-11-23 20:31:21 -0500

    swagger install

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'Animals Api',
+        description: 'Animals Api'
+    },
+    host: 'localhost:3030',
+    schemes: ['https', 'http'
+};
+
+const outputFile = './swagger.json';
+const endpointsFile = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointsFile, doc);
