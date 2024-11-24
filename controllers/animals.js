46d66cb63fd2b9f8e2e665df2ef4c7b929a2acc7
Tara Bergener <tara.kelly211@gmail.com>
2024-11-23 22:14:52 -0500

    delete update

--- controllers/animals.js
+++ controllers/animals.js
@@ -76,11 +76,11 @@ const updateAnimal = async (req, res) => {
 };
 
 const deleteAnimal = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid contact id to delete a contact.');
-    }
+    //if (!ObjectId.isValid(req.params.id)) {
+    //    res.status(400).json('Must use a valid contact id to delete a contact.');
+    //}
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('animals').remove({ _id: userId }, true);
+    const response = await mongodb.getDatabase().db().collection('animals').deleteOne({ _id: userId });
     if (response.deleteCount > 0) {
         res.status(204).send();
     } else {
