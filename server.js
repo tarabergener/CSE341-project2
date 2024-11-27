588b4c24b778c9d62d547930dc6a477f59348c5b
Tara Bergener <tara.kelly211@gmail.com>
2024-11-27 09:15:38 -0500

    OAuth changes and updates

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 
 const port = process.env.PORT || 3001;
+const app = express();
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    //res.setHeader(
-    //    'Access-Control-Allow-Headers',
-    //    'Origin, X-Requested-With, Content-Type, Accept, 2-Key'
-    //);
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+app
+    .use(session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true
+    }))
+    //This is the basic express session((..)) initialization
+    .use(passport.initialize())
+    //init passport on every route call
+    .use(passport.session())
+    //allow passport to use "express-session"
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, 2-Key'
+        );
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
-});
+    })
+    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
+    .use(corse({ origin: '*'}))
+    .use('/', require('./routes/index.js'))
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done) {
+  //User.findOrCreat({ githubId: profile.id }, function (err, user) {  
+    return done(null, profile);
+  //})  
+}
+))
 
 app.use('/', require('./routes'));
 
