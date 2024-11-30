0519ca2efed0c2d522f28f25a7aa38c0e80cccaa
Tara Bergener <tara.kelly211@gmail.com>
2024-11-30 13:00:44 -0500

    get all change

--- controllers/animals.js
+++ controllers/animals.js
@@ -5,7 +5,7 @@ const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('animals').find();
     result.toArray().then((animals) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(animals[0]);
+        res.status(200).json(animals);
     });
 };
 
