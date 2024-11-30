939e2ede89fb157369ab43ebba9bfee158d8da2f
Tara Bergener <tara.kelly211@gmail.com>
2024-11-30 13:29:13 -0500

    get all experiment

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,16 +1,20 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('animals').find();
-    result.toArray().then((animals) => {
+const getAll = (req, res) => {
+    mongodb
+      .getDatabase()
+      .db()
+      .collection('animals')
+      .find()
+      .toArray((err, animals) => {
         if (err) {
-            res.status(400).json({ message: err });
+          res.status(400).json({ message: err });
         }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(animals);
-    });
-};
+      });
+  };
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['Animals']
